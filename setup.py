cba4541c579ad22b99ca0143365e0126fadb22da
Pavol Gajdoš <pavolgaj@users.noreply.github.com>
2017-04-03 16:22:10 +0200

    Add files via upload
    Setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+try: 
+      from setuptools import setup
+except ImportError:
+      from distutils.core import setup
+    
+import sys
+
+py_version = sys.version_info[:2]
+
+if not py_version==(2,7):	
+      raise RuntimeError('Python 2.7 is required!	')	
+
+#requires=['numpy==1.10.2','matplotlib==1.5.0','PyAstronomy==0.9.0','pymc==2.3.6']
+
+dist=setup(name="OCFit",
+      version="0.1.1",
+      description="Fitting O-C diagrams",
+      author="Pavol Gajdos",
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Astronomy"],
+      url='https://github.com/pavolgaj/OCFit',
+      install_requires=['numpy>=1.10.2','matplotlib>=1.5.0','PyAstronomy>=0.9.0'],
+      extras_require={'MonteCarlo Fitting':  ['pymc>=2.3.6']},
+      py_modules=["OCFit/__init__","OCFit/OC_class","OCFit/info_ga","OCFit/ga"]
+) 
