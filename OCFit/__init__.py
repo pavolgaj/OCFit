3889a47b25d57ab857bb050f225838f8ae183366
Pavol Gajdoš <pavolgaj@users.noreply.github.com>
2017-10-23 12:25:34 +0200

    Add files via upload

---
+++ OCFit/__init__.py
@@ -0,0 +1,10 @@
+from OC_class import FitLinear
+from OC_class import FitQuad
+from OC_class import OCFit
+from OC_class import OCFitLoad
+
+__version__='0.1.1'
+
+del ga
+del OC_class
+#del info_ga
