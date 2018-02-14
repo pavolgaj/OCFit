f46eca8f2bdd92acba42f2228fd3411229849a51
Pavol Gajdoš <pavol1gajdos@gmail.com>
2018-02-14 14:49:14 +0100

    Add files via upload

---
+++ examples/fit_example.py
@@ -0,0 +1,45 @@
+from OCFit import OCFit, FitLinear
+import numpy as np
+
+#generating data
+E = np.arange(0, 100, 1)  #epochs
+#simulation of observed times, LiTE with amplitude 72 min. and period 1125 d
+P = 15
+t0 = 1540
+t = P*E + t0 + 0.05*np.sin(2*np.pi/(5*P)*E)
+    + np.random.normal(scale = 0.01, size = E.shape) 
+err = 0.01*np.ones(E.shape)  #errors of 'observed' times
+
+#usage of FitLinear for calculation of O-C
+#initialization using estiamtion (original values) of linear ephemeris
+#for long time range of observation, the value of period has to be close
+#to the right value, otherwise the primary and secondary minims could be mixed
+lin = FitLinear(t, t0, P, err = err)
+oc = lin.oc  #O-C calculated from original ephemeris
+
+#initialization of class OCFit and O-C calculated using FitLinear
+fit=OCFit(t,oc,err = err)
+fit.Epoch(t0,P)  #calculating epochs
+
+#setting the model and parameters for fitting
+fit.model='LiTE3'
+fit.fit_params=['a_sin_i3', 'e3', 'w3', 't03', 'P3']
+fit.limits={'a_sin_i3': [5, 10], 'e3': [0, 1], 'w3': [0, 2*np.pi],
+            't03': [1540, 3000], 'P3': [900, 1300]}
+fit.steps={'a_sin_i3': 1e-2, 'e3': 1e-2, 'w3': 1e-2, 't03': 10, 'P3': 10}
+
+#fitting using GA without displaying fitting progress
+fit.FitGA(100,100,visible=False)
+#sumarry of results after GA
+fit.Summary()
+
+#fitting using MCMC without displaying fitting progress
+fit.FitMCMC(1e3,visible=False)
+#sumarry of results after MC
+fit.Summary() 
+
+#plotting figure
+#figure with original O-C with fit without transformation of x axis
+#together with residue and 2nd axis in epochs
+fit.Plot(trans = False,with_res=True,double_ax=True)
+
---
+++ examples/fit_quad_example.py
@@ -0,0 +1,46 @@
+from OCFit import FitQuad, FitLinear
+import numpy as np
+
+#generate data
+E = np.arange(0, 100, 1)  #epochs
+#simulation of observed times
+t = 1e-5*E**2+15*E + 1540.4 + np.random.normal(scale = 0.01, size = E.shape) 
+err = 0.01*np.ones(E.shape)  #errors of 'observed' times
+
+#usage of FitLinear to first estimation of linear ephemeris
+#initialization using estiamtion (original values) of linear ephemeris
+#for long time range of observation, the value of period has to be close
+#to the right value, otherwise the primary and secondary minims could be mixed
+lin = FitLinear(t, 1540, 15.01, err = err)
+oc = lin.oc  #O-C calculated from original ephemeris
+lin.FitLinear()  #linear fit
+lin.FitRobust()  #fitting using robust regression
+
+lin.Summary() #summary of parameters
+
+err = lin.CorrectErr() #re-normalization of errors
+oc = lin.FitRobust()  #fitting using robust regression
+
+lin.Summary() #summary of parameters
+
+#plotting figures
+#plot of original values of O-C with fit, without transformation of x axis
+lin.Plot(trans = False)
+#plot of residual O-C, without transformation of x axis
+lin.PlotRes(trans = False)   
+
+#usage of class FitQuad
+#as estimation of linear ephemeris the resault from FitLinear are used
+#new values of O-C are only fitted
+quad = FitQuad(t, lin.t0, lin.P, oc = oc, err = err)
+oc = quad.oc  #O-C calculated from original ephemeris
+quad.FitQuad()  #quadratic fit
+quad.FitRobust()  #fitting using robust regression
+
+quad.Summary() #summary of parameters
+
+#plotting figures
+#plot of original values of O-C with fit, without transformation of x axis
+quad.Plot(trans = False)
+#plot of residual O-C, without transformation of x axis
+quad.PlotRes(trans = False)   
