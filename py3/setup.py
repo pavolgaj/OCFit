c6a43cd01624f2271bbd349b4506b9ae762b6228
Pavol Gajdoš <pavol1gajdos@gmail.com>
2019-02-18 18:29:59 +0100

    Update setup.py

--- py3/setup.py
+++ py3/setup.py
@@ -1,16 +1,11 @@
-try: 
-      from setuptools import setup
-except ImportError:
-      from distutils.core import setup
+try: from setuptools import setup
+except ImportError: from distutils.core import setup
     
 import sys
 
 py_version = sys.version_info[0]
 
-if not py_version==(3):	
-      raise RuntimeError('Python 3 is required!	')	
-
-#requires=['numpy==1.10.2','matplotlib==1.5.0','PyAstronomy==0.9.0','pymc==2.3.6']
+if not py_version==(3):	raise RuntimeError('Python 3 is required!	')
 
 dist=setup(name="OCFit",
       version="0.1.2",
@@ -24,4 +19,4 @@ dist=setup(name="OCFit",
       install_requires=['numpy>=1.10.2','matplotlib>=1.5.0','PyAstronomy>=0.9.0'],
       extras_require={'MonteCarlo Fitting':  ['pymc>=2.3.6']},
       py_modules=["OCFit/__init__","OCFit/OC_class","OCFit/info_mc","OCFit/info_ga","OCFit/ga"]
-) 
+)
