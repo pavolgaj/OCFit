cf58eb44a179adb5d760ab1b58b3db5cc88b8cb6
Pavol Gajdoš <pavol1gajdos@gmail.com>
2025-10-19 16:59:53 +0200

    numpy v2 compatibility

--- OCFit/OC_class.py
+++ OCFit/OC_class.py
@@ -70,7 +70,7 @@ class _Prior(object):
         def unilimit(ps, n, **rest):
             if (ps[n] >= kwargs["lower"]) and (ps[n] <= kwargs["upper"]):
                 return p
-            else: return -np.Inf
+            else: return -np.inf
         return unilimit
 
     def __call__(self, *args, **kwargs):
@@ -82,16 +82,16 @@ class _Prior(object):
 class _NumpyEncoder(json.JSONEncoder):
     """ Custom encoder for numpy data types """
     def default(self, obj):
-        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+        if isinstance(obj, (np.intc, np.intp, np.int8,
                             np.int16, np.int32, np.int64, np.uint8,
                             np.uint16, np.uint32, np.uint64)):
 
             return int(obj)
 
-        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+        elif isinstance(obj, (np.float16, np.float32, np.float64)):
             return float(obj)
 
-        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+        elif isinstance(obj, (np.complex64, np.complex128)):
             return {'real': obj.real, 'imag': obj.imag}
 
         elif isinstance(obj, (np.ndarray,)):
